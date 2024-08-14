709063c1d5fd998bd4ac60c2d9fb887327b52b60
v.ivanova <viktoriia.ivanova@vista.com>
2024-08-15 00:30:43 +0300

    fetchData.ts

--- src/utilities/fetchData.ts
+++ src/utilities/fetchData.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const URL = 'https://api.unsplash.com/search/photos'
 const API_KEY = 'JAcvmRB72np9Zb0uw-aN8WLGkxNdyXyW6XRPL8pzTfw';
 
-export default async function fetchData(topic, page=1) {
-  const response = await axios.get(URL, {
+export default async function fetchData(topic: string, page=1): Promise<Response>  {
+  const response = await axios.get<Response>(URL, {
     params: {
       client_id: API_KEY,
       query: topic,
